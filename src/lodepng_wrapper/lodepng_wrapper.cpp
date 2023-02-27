5933d26f108b9a90e108cf90f2ce4a230e3942ce
eersaa <eerik.saarinen@gmail.com>
2023-02-27 19:14:21 +0200

    Create first method for wrapper

--- src/lodepng_wrapper/lodepng_wrapper.cpp
+++ src/lodepng_wrapper/lodepng_wrapper.cpp
@@ -1,2 +1,14 @@
+// Wrapper created based on the example from the lodepng library
 #include "lodepng_wrapper.h"
 
+using namespace lodepng_wrapper;
+
+LodepngWrapper::LodepngWrapper()
+{
+    ;
+}
+
+unsigned LodepngWrapper::load_image(const std::string& filename)
+{
+    return lodepng::decode(image, width, height, filename);
+}
--- src/lodepng_wrapper/lodepng_wrapper.h
+++ src/lodepng_wrapper/lodepng_wrapper.h
@@ -1,6 +1,24 @@
+// Wrapper created based on the example from the lodepng library
 #ifndef LODEPNG_WRAPPER_HPP
 #define LODEPNG_WRAPPER_HPP
 
 #include "lodepng.h"
+
+namespace lodepng_wrapper
+{
+
+class LodepngWrapper
+{
+    public:
+    LodepngWrapper();
+    unsigned load_image(const std::string& filename);
+
+    private:
+    std::vector<unsigned char> image;
+    unsigned width;
+    unsigned height;
+};
+
+} // namespace lodepng_wrapper
 
 #endif // LODEPNG_WRAPPER_HPP
