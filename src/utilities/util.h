6618719367c5561ac397798225dcdd283bb13982
eersaa <eerik.saarinen@gmail.com>
2023-03-07 16:14:59 +0200

    Put define before the include of OpenCL
    This prevents the warning message "#pragma message: cl_version.h: CL_TARGET_OPENCL_VERSION is not defined.
    Defaulting to 220 (OpenCL 2.2)"

--- src/utilities/util.h
+++ src/utilities/util.h
@@ -2,10 +2,13 @@
 #define UTIL_H
 #include <stdio.h>
 #include <stdlib.h>
-#include <CL/cl.h>
 
+#ifndef CL_TARGET_OPENCL_VERSION
 #define CL_TARGET_OPENCL_VERSION 120
+#endif
+#include <CL/opencl.h>
 
-int printPlatformProfile ()
+
+int printPlatformProfile();
 
 #endif  // UTIL_H
