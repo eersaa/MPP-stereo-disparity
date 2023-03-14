fb3e53bb74d2ef3f3e8e04f2c4e5e4b798e08691
eersaa <eerik.saarinen@gmail.com>
2023-03-14 18:38:44 +0200

    Add Run() method

--- src/opencl_hello_world/hello-world.h
+++ src/opencl_hello_world/hello-world.h
@@ -5,6 +5,7 @@
 #define CL_TARGET_OPENCL_VERSION 120
 #endif
 #include <CL/opencl.h>
+
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,13 +13,26 @@
 #include <string>
 #include <fstream>
 
+#include "opencl_base.h"
+
 class OCL_HelloWorld : public OCL_Base
 {
 public:
     OCL_HelloWorld();
     ~OCL_HelloWorld();
 
+    void Run() override;
 private:
+
+    cl_kernel kernel;
+    cl_mem inputBuffer;
+    cl_mem outputBuffer;
+
+    const char* input;
+    char* output;
+
+    int inputSize;
+    int outputSize;
 
 };
 
