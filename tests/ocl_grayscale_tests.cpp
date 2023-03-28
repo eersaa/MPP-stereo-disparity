9f495553a7b0352cb89f7a15d1c35d426a306f56
eersaa <eerik.saarinen@gmail.com>
2023-03-28 22:21:46 +0300

    Test with real image

--- tests/ocl_grayscale_tests.cpp
+++ tests/ocl_grayscale_tests.cpp
@@ -244,3 +244,26 @@ TEST_F(OCL_GrayscaleTwoPixelTests, ShouldKeepAlphaChannelTheSame)
     ocl_grayscale.Convert_RGBA(image, result_image, pixels);
     EXPECT_THAT(result_image[7], Eq(image[7]));
 }
+
+#include "lodepng_wrapper.h"
+
+TEST(OCL_GrayscaleRealImage, ShouldReturnGrayscaledImage)
+{
+    lodepng_wrapper::LodepngWrapper img0;
+    unsigned error = img0.load_image("../../source-img/im0.png");
+    unsigned width = img0.get_width();
+    unsigned height = img0.get_height();
+    unsigned char *image = (unsigned char *)malloc(width * height * 4 * sizeof(unsigned char));
+    img0.clone_image(image);
+
+    OCL_Grayscale ocl_grayscale;
+    unsigned char *result_image = (unsigned char *)malloc(width * height * 4 * sizeof(unsigned char));
+    ocl_grayscale.Convert_RGBA(image, result_image, width * height);
+
+    lodepng_wrapper::LodepngWrapper img1;
+    img1.set_image(result_image, width, height);
+    img1.save_RGBAimage("../../output-img/ocl_grayscale.png");
+
+    free(image);
+    free(result_image);
+}
