8628dbb5e69f865c41e50c6d60085c53fcd1c196
eersaa <eerik.saarinen@gmail.com>
2023-03-31 12:40:58 +0300

    Ref: Add pixel arguments

--- tests/cross_checking_tests.cpp
+++ tests/cross_checking_tests.cpp
@@ -1,7 +1,7 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-int crossCheck()
+int crossCheck(int pixel1, int pixel2)
 {
     return 0;
 }
@@ -10,5 +10,7 @@ using namespace testing;
 
 TEST(CrossCheckingTests, ShouldReturnZeroOutputPixelGivenTwoZeroInputPixels)
 {
-    ASSERT_THAT(crossCheck(), Eq(0));
+    int pixel1 = 0;
+    int pixel2 = 0;
+    ASSERT_THAT(crossCheck(pixel1, pixel2), Eq(0));
 }
