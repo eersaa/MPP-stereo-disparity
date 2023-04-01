a867a8fe7266f7e8884e2ca80c0c711085894bb6
eersaa <eerik.saarinen@gmail.com>
2023-04-01 15:14:40 +0300

    Ref: Refactor test suite names

--- tests/occlusion_fill_tests.cpp
+++ tests/occlusion_fill_tests.cpp
@@ -24,7 +24,7 @@ int occlusionFill(int pixel_index, int *image, int width)
 
 using namespace testing;
 
-TEST(OcclusionFillTests, ShouldReturnPixelWith1GivenOnePixelWith0Value)
+TEST(OcclusionFillOnePixelTest, ShouldReturnPixelWith1GivenPixelWith0Value)
 {
     int image[1] = {0};
     int width = 1;
@@ -32,18 +32,22 @@ TEST(OcclusionFillTests, ShouldReturnPixelWith1GivenOnePixelWith0Value)
     ASSERT_THAT(occlusionFill(pixelIndex, image, width), Eq(1));
 }
 
-TEST(OcclusionFillTests, ShouldReturnPixelValueOnRightSideOfZeroPixel)
+class OcclusionFillTwoPixelTests : public ::testing::Test
+{
+public:
+    int width = 2;
+};
+
+TEST_F(OcclusionFillTwoPixelTests, ShouldReturnPixelValueOnRightSideOfZeroPixel)
 {
     int image[2] = {0, 2};
-    int width = 2;
     int pixelIndex = 0;
     ASSERT_THAT(occlusionFill(pixelIndex, image, width), Eq(2));
 }
 
-TEST(OcclusionFillTests, ShouldReturnPixelValueOnLeftSideOfZeroPixel)
+TEST_F(OcclusionFillTwoPixelTests, ShouldReturnPixelValueOnLeftSideOfZeroPixel)
 {
     int image[2] = {2, 0};
-    int width = 2;
     int pixelIndex = 1;
     ASSERT_THAT(occlusionFill(pixelIndex, image, width), Eq(2));
 }
