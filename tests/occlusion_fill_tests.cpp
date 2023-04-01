c676df509d2acd7117d4734ac38669588b522b27
eersaa <eerik.saarinen@gmail.com>
2023-04-01 14:32:44 +0300

    Add test ShouldReturnPixelValueOnRightSideOfZeroPixel

--- tests/occlusion_fill_tests.cpp
+++ tests/occlusion_fill_tests.cpp
@@ -1,20 +1,25 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-int occlusionFill(int pixel)
+int occlusionFill(int *pixel)
 {
-    if (pixel == 0)
+    if (*pixel == 0)
     {
         return 1;
     }
     return 0;
 }
 
-
 using namespace testing;
 
 TEST(OcclusionFillTests, ShouldReturnPixelWith1GivenOnePixelWith0Value)
 {
-    int pixel = 0;
+    int pixel[1] = {0};
     ASSERT_THAT(occlusionFill(pixel), Eq(1));
+}
+
+TEST(OcclusionFillTests, ShouldReturnPixelValueOnRightSideOfZeroPixel)
+{
+    int pixel[2] = {0, 2};
+    ASSERT_THAT(occlusionFill(pixel), Eq(2));
 }
