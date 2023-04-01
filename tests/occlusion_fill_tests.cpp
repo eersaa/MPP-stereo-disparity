7ef0739794ffe56896939b7e94cf8c506727a483
eersaa <eerik.saarinen@gmail.com>
2023-04-01 15:01:07 +0300

    Add test ShouldReturnPixelValueOnLeftSideOfZeroPixel

--- tests/occlusion_fill_tests.cpp
+++ tests/occlusion_fill_tests.cpp
@@ -1,17 +1,18 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-int occlusionFill(int *image, int width)
+int occlusionFill(int pixel_index, int *image, int width)
 {
+    int fillPixelValue = 0;
     if (width == 2)
     {
-        return *(image + 1);
+        fillPixelValue = *(image + 1);
     }
-    if (*image == 0)
+    if (fillPixelValue == 0)
     {
-        return 1;
+        fillPixelValue = 1;
     }
-    return 0;
+    return fillPixelValue;
 }
 
 using namespace testing;
@@ -20,12 +21,22 @@ TEST(OcclusionFillTests, ShouldReturnPixelWith1GivenOnePixelWith0Value)
 {
     int image[1] = {0};
     int width = 1;
-    ASSERT_THAT(occlusionFill(image, width), Eq(1));
+    int pixelIndex = 0;
+    ASSERT_THAT(occlusionFill(pixelIndex, image, width), Eq(1));
 }
 
 TEST(OcclusionFillTests, ShouldReturnPixelValueOnRightSideOfZeroPixel)
 {
     int image[2] = {0, 2};
     int width = 2;
-    ASSERT_THAT(occlusionFill(image, width), Eq(2));
+    int pixelIndex = 0;
+    ASSERT_THAT(occlusionFill(pixelIndex, image, width), Eq(2));
+}
+
+TEST(OcclusionFillTests, ShouldReturnPixelValueOnLeftSideOfZeroPixel)
+{
+    int image[2] = {2, 0};
+    int width = 2;
+    int pixelIndex = 1;
+    ASSERT_THAT(occlusionFill(pixelIndex, image, width), Eq(2));
 }
