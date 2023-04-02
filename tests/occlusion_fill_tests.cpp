a280494a4831fa1f6daa4b7b5efb26cdc4ed3a55
eersaa <eerik.saarinen@gmail.com>
2023-04-02 15:30:37 +0300

    Add test ShouldReturnPixelValueAboveZeroPixel and...
    create test fixture OcclusionFillTwoVerticalPixelsTests

--- tests/occlusion_fill_tests.cpp
+++ tests/occlusion_fill_tests.cpp
@@ -71,11 +71,23 @@ TEST_F(OcclusionFillThreePixelTests, ShouldReturnPixelValueOnLeftWhenMiddleIndex
 }
 
 
-TEST(OcclusionFillTwoVerticalPixelsTests, ShouldReturnPixelValueBelowZeroPixel)
+class OcclusionFillTwoVerticalPixelsTests : public ::testing::Test
 {
-    int image[2] = {0, 2};
+public:
     int width = 1;
     int height = 2;
+};
+
+TEST_F(OcclusionFillTwoVerticalPixelsTests, ShouldReturnPixelValueBelowZeroPixel)
+{
+    int image[2] = {0, 2};
     int pixelIndex = 0;
+    ASSERT_THAT(occlusionFill(pixelIndex, image, width, height), Eq(2));
+}
+
+TEST_F(OcclusionFillTwoVerticalPixelsTests, ShouldReturnPixelValueAboveZeroPixel)
+{
+    int image[2] = {2, 0};
+    int pixelIndex = 1;
     ASSERT_THAT(occlusionFill(pixelIndex, image, width, height), Eq(2));
 }
