dd8b9f5fc331939862679d655349c03bc066145e
eersaa <eerik.saarinen@gmail.com>
2023-04-02 14:29:58 +0300

    Move occlusion_fill code to a separate file

--- tests/occlusion_fill_tests.cpp
+++ tests/occlusion_fill_tests.cpp
@@ -1,27 +1,7 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include "occlusion_fill.h"
 
-int occlusionFill(int pixel_index, int *image, int width)
-{
-    int fillPixelValue = 0;
-    int leftRange = 0 - pixel_index;
-    int rightRange = width - pixel_index;
-
-    for (int i = leftRange; i < rightRange; i++)
-    {
-        if (*(image + pixel_index + i) != 0)
-        {
-            fillPixelValue = *(image + pixel_index + i);
-            break;
-        }
-    }
-
-    if (fillPixelValue == 0)
-    {
-        fillPixelValue = 1;
-    }
-    return fillPixelValue;
-}
 
 using namespace testing;
 
