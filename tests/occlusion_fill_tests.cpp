f38a4cc6c1751da0b30c0b2df0c0aebf472837a1
eersaa <eerik.saarinen@gmail.com>
2023-04-05 20:21:39 +0300

    G: Pass test ShouldReturnPixelValueOnTopOfZeroPixel
    In OcclusionFillTwoVerticalPixelTests suite.

--- tests/occlusion_fill_tests.cpp
+++ tests/occlusion_fill_tests.cpp
@@ -1,10 +1,10 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-int occlusionFill(int pixel_index, int *image, int width)
+int occlusionFill(int pixel_index, int *image, int width, int height)
 {
     int fillPixelValue = 0;
-    if (width == 2)
+    if (width == 2 || height == 2)
     {
         if (pixel_index == 0)
         {
@@ -28,26 +28,37 @@ TEST(OcclusionFillOnePixelTest, ShouldReturnPixelWith1GivenPixelWith0Value)
 {
     int image[1] = {0};
     int width = 1;
+    int height = 1;
     int pixelIndex = 0;
-    ASSERT_THAT(occlusionFill(pixelIndex, image, width), Eq(1));
+    ASSERT_THAT(occlusionFill(pixelIndex, image, width, height), Eq(1));
 }
 
 class OcclusionFillTwoPixelTests : public ::testing::Test
 {
 public:
     int width = 2;
+    int height = 1;
 };
 
 TEST_F(OcclusionFillTwoPixelTests, ShouldReturnPixelValueOnRightSideOfZeroPixel)
 {
     int image[2] = {0, 2};
     int pixelIndex = 0;
-    ASSERT_THAT(occlusionFill(pixelIndex, image, width), Eq(2));
+    ASSERT_THAT(occlusionFill(pixelIndex, image, width, height), Eq(2));
 }
 
 TEST_F(OcclusionFillTwoPixelTests, ShouldReturnPixelValueOnLeftSideOfZeroPixel)
 {
     int image[2] = {2, 0};
     int pixelIndex = 1;
-    ASSERT_THAT(occlusionFill(pixelIndex, image, width), Eq(2));
+    ASSERT_THAT(occlusionFill(pixelIndex, image, width, height), Eq(2));
+}
+
+TEST(OcclusionFillTwoVerticalPixelTests, ShouldReturnPixelValueOnTopOfZeroPixel)
+{
+    int image[2] = {2, 0};
+    int width = 1;
+    int height = 2;
+    int pixelIndex = 1;
+    ASSERT_THAT(occlusionFill(pixelIndex, image, width, height), Eq(2));
 }
