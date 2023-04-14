f4068c4623b03ec26d6cc2c4507bc31a8a347c1d
eersaa <eerik.saarinen@gmail.com>
2023-04-14 15:54:20 +0300

    Move occlusion_fill program to separate file

---
+++ tests/occlusion_fill.h
@@ -0,0 +1,59 @@
+int pixelRow(int pixel_index, int width)
+{
+    return pixel_index / width;
+}
+
+int verDistance(int pixel_index, int row_index, int width)
+{
+    return abs(pixelRow(pixel_index, width) - row_index);
+}
+
+int pixelColumn(int pixel_index, int height)
+{
+    return pixel_index / height;
+}
+
+int horDistance(int pixel_index, int column_index, int height)
+{
+    return abs(pixelColumn(pixel_index, height) - column_index);
+}
+
+int distance(int pixel_index, int index)
+{
+    return abs(pixel_index - index);
+}
+
+double euclideanDistance(int pixel_index, int x_index, int y_index)
+{
+    return sqrt(pow(distance(pixel_index, x_index), 2) + pow(distance(pixel_index, y_index), 2));
+}
+
+int occlusionFill(int pixel_index, int *image, int width, int height)
+{
+    int fillPixelValue = 0;
+    int pixelValue = 0;
+
+    double minDist = width + height;
+
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            double dist = euclideanDistance(pixel_index, x, y);
+            pixelValue = *(image + x + y * width);
+
+            if (pixelValue != 0
+                && dist < minDist)
+            {
+                minDist = dist;
+                fillPixelValue = pixelValue;
+            }
+        }
+    }    
+    
+    if (fillPixelValue == 0)
+    {
+        fillPixelValue = 1;
+    }
+    return fillPixelValue;
+}
