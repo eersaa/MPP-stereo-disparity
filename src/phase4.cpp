e920c1e87e736496ee8331b91fc8fa83d5c480ec
eersaa <eerik.saarinen@gmail.com>
2023-04-29 20:46:31 +0300

    Separate image cloning from OpenCL environment to pointer

--- src/phase4.cpp
+++ src/phase4.cpp
@@ -9,17 +9,27 @@ int scaling_factor = 4;
 class OCL_Phase4 : public OCL_Base
 {
 public:
+    OCL_Phase4() : OCL_Base()
+    {
+    }
+
+    ~OCL_Phase4()
+    {
+        clReleaseMemObject(inputBuffer);
+        clReleaseMemObject(outputBuffer);
+    }
+
     void Run() override
     {
     }
 
-    void Convert_RGBA_to_grayscale(unsigned char *image, unsigned char *result_image, int pixels)
+    void Convert_RGBA_to_grayscale(unsigned char *image, int width, int height)
     {
 
-        cl_mem inputBuffer = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
-                                            pixels * 4 * sizeof(unsigned char), image, NULL);
-        cl_mem outputBuffer = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
-                                            pixels * 4 * sizeof(unsigned char), NULL, NULL);
+        inputBuffer = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+                                            width * height * 4 * sizeof(unsigned char), image, NULL);
+        outputBuffer = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
+                                            width * height * 4 * sizeof(unsigned char), NULL, NULL);
 
         cl_program prog = CreateProgramFromFile("kernels/grayscale.cl");
         CreateKernelFromProgram(prog, "grayscale_rgba");
@@ -27,14 +37,21 @@ public:
         clSetKernelArg(GetKernel(0), 0, sizeof(cl_mem), (void *)&inputBuffer);
         clSetKernelArg(GetKernel(0), 1, sizeof(cl_mem), (void *)&outputBuffer);
 
-        size_t global_work_size[1] = {pixels * 4 * sizeof(unsigned char)};
+        size_t global_work_size[1] = {width * height * 4 * sizeof(unsigned char)};
         clEnqueueNDRangeKernel(commandQueue, GetKernel(0), 1, NULL,
                             global_work_size, NULL, 0, NULL, NULL);
 
-        clEnqueueReadBuffer(commandQueue, outputBuffer, CL_TRUE, 0,
-                            pixels * 4 * sizeof(unsigned char), result_image, 0, NULL, NULL);
     }
 
+    void clone_image(unsigned char *dest, int width, int height)
+    {
+        clEnqueueReadBuffer(commandQueue, outputBuffer, CL_TRUE, 0,
+                    width * height * 4 * sizeof(unsigned char), dest, 0, NULL, NULL);
+    }
+
+private:
+    cl_mem inputBuffer;
+    cl_mem outputBuffer;
 };
 
 OCL_Phase4 ocl_phase4;
@@ -44,7 +61,7 @@ struct LoadImage : public IProgram
     int run() override
     {
         unsigned error = img0.load_image("../../source-img/im0.png");
-        error = img1.load_image("../../source-img/im1.png");
+        // error = img1.load_image("../../source-img/im1.png");
         return (int) error;
     }
 };
@@ -66,15 +83,11 @@ struct TransformToGreyscale : public IProgram
         unsigned width = img0.get_width();
         unsigned height = img0.get_height();
         unsigned char *RGBA_image = (unsigned char *)malloc(width * height * 4 * sizeof(unsigned char));
-        unsigned char *grayscale_image = (unsigned char *)malloc(width * height * 4 * sizeof(unsigned char));
 
         img0.clone_image(RGBA_image);
-        ocl_phase4.Convert_RGBA_to_grayscale(RGBA_image, grayscale_image, width * height);
-
-        img0.set_image(grayscale_image, width, height, 4);
+        ocl_phase4.Convert_RGBA_to_grayscale(RGBA_image, width, height);
 
         free(RGBA_image);
-        free(grayscale_image);
         return 0;
     }
 };
@@ -83,8 +96,17 @@ struct SaveGreyscaleImage : public IProgram
 {
     int run() override
     {
+        unsigned width = img0.get_width();
+        unsigned height = img0.get_height();
+        unsigned char *grayscale_image = (unsigned char *)malloc(width
+                                                            * height 
+                                                            * 4 * sizeof(unsigned char));
+
+        ocl_phase4.clone_image(grayscale_image, width, height);
+        img0.set_image(grayscale_image, width, height, 4);
         unsigned error = img0.save_image("../../output-img/im0_grey.png");
-        error = img1.save_image("../../output-img/im1_grey.png");
+        // error = img1.save_image("../../output-img/im1_grey.png");
+        free(grayscale_image);
         return (int) error;
     }
 };
