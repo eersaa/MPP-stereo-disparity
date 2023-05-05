bb5b3fd2481faf7a97dc3a43f563fe7527d02b1c
Mikael <32382429+NortHund@users.noreply.github.com>
2023-05-05 16:35:20 +0300

    Step 3 functionality of Phase 1 added.

--- src/phase1.cpp
+++ src/phase1.cpp
@@ -1,6 +1,10 @@
 #include "phase1.h"
 
 lodepng_wrapper::LodepngWrapper img0;
+//lodepng_wrapper::LodepngWrapper img1;
+
+int scaling_factor = 4;
+int avg_window_size = 5;
 
 int matwidth = 100;
 int matheight = 100;
@@ -23,12 +27,174 @@ unsigned long get_kernel_execution_time(cl_event &event, cl_command_queue &comma
     return (time_end - time_start) / 1000;
 }
 
+class OCL_image : public lodepng_wrapper::LodepngWrapper
+{
+public:
+    OCL_image(std::unique_ptr<OCL_Base>& ocl_base)
+        : _ocl_base(ocl_base)
+    {
+    }
+
+    ~OCL_image()
+    {
+        if(imageBuffer != nullptr) clReleaseMemObject(imageBuffer);
+        if(averageBuffer != nullptr) clReleaseMemObject(averageBuffer);
+    }
+
+    unsigned transform_to_grayscale() override
+    {
+        cl_mem inputBuffer = clCreateBuffer(_ocl_base->context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+                                            _width * _height * 4 * sizeof(unsigned char), _image, NULL);
+        imageBuffer = clCreateBuffer(_ocl_base->context, CL_MEM_READ_WRITE,
+                                            _width * _height * sizeof(unsigned char), NULL, NULL);
+
+        clSetKernelArg(_ocl_base->GetKernel(0), 0, sizeof(cl_mem), (void *)&inputBuffer);
+        clSetKernelArg(_ocl_base->GetKernel(0), 1, sizeof(cl_mem), (void *)&imageBuffer);
+
+        size_t global_work_size[1] = {_width * _height * 4 * sizeof(unsigned char)};
+        clEnqueueNDRangeKernel(_ocl_base->commandQueue, _ocl_base->GetKernel(0), 1, NULL,
+                            global_work_size, NULL, 0, NULL, &_event);
+
+        kernel_execution_times[0] = get_kernel_execution_time(_event, _ocl_base->commandQueue);
+
+        clReleaseMemObject(inputBuffer);
+
+        _imageIsGrayscaled = true;
+
+        return 0;
+    }
+
+    void clone_image(unsigned char *dest) override
+    {
+        clEnqueueReadBuffer(_ocl_base->commandQueue, imageBuffer, CL_TRUE, 0,
+                    _width * _height * sizeof(unsigned char), dest, 0, NULL, NULL);
+    }
+
+    unsigned resize_image(unsigned scalingFactor) override
+    {
+        _width = _width / scalingFactor;
+        _height = _height / scalingFactor;
+
+        cl_int status;
+        status = clSetKernelArg(_ocl_base->GetKernel(1), 0, sizeof(scalingFactor), &scalingFactor);
+        status = clSetKernelArg(_ocl_base->GetKernel(1), 1, sizeof(cl_mem), (void *)&imageBuffer);
+        status = clSetKernelArg(_ocl_base->GetKernel(1), 2, sizeof(cl_mem), (void *)&imageBuffer);
+
+        size_t global_work_size[2];
+        global_work_size[0] = _width * sizeof(unsigned char);
+        global_work_size[1] = _height * sizeof(unsigned char);
+
+        clEnqueueNDRangeKernel(_ocl_base->commandQueue, _ocl_base->GetKernel(1), 2, NULL,
+                            global_work_size, NULL, 0, NULL, &_event);
+
+        kernel_execution_times[1] = get_kernel_execution_time(_event, _ocl_base->commandQueue);
+
+        return (unsigned)status;
+    }
+
+    unsigned average(int windowSize)
+    {
+        averageBuffer = clCreateBuffer(_ocl_base->context,
+                                        CL_MEM_READ_WRITE,
+                                        _width * _height * sizeof(unsigned char),
+                                        NULL,
+                                        NULL);
+
+
+        cl_int status;
+        status = clSetKernelArg(_ocl_base->GetKernel(2), 0, sizeof(cl_mem), (void *)&imageBuffer);
+        status = clSetKernelArg(_ocl_base->GetKernel(2), 1, sizeof(cl_mem), (void *)&averageBuffer);
+        status = clSetKernelArg(_ocl_base->GetKernel(2), 2, sizeof(int), &windowSize);
+
+        size_t global_work_size[2];
+        global_work_size[0] = _width * sizeof(unsigned char);
+        global_work_size[1] = _height * sizeof(unsigned char);
+
+        status = clEnqueueNDRangeKernel(_ocl_base->commandQueue,
+                                        _ocl_base->GetKernel(2),
+                                        2,
+                                        NULL,
+                                        global_work_size,
+                                        NULL,
+                                        0,
+                                        NULL,
+                                        &_event);
+
+        kernel_execution_times[2] = get_kernel_execution_time(_event, _ocl_base->commandQueue);
+
+        status = clEnqueueCopyBuffer(_ocl_base->commandQueue,
+                            averageBuffer,
+                            imageBuffer,
+                            0,
+                            0,
+                            _width * _height * sizeof(unsigned char),
+                            0,
+                            NULL,
+                            NULL);
+
+        return (unsigned)status;
+    }
+
+    unsigned save_image(const char* filename) override
+    {
+        if(_imageIsGrayscaled)
+        {
+            _image = (unsigned char *)realloc(_image, _width * _height * sizeof(unsigned char));
+            clone_image(_image);
+            return LodepngWrapper::save_image(filename);
+        }
+        else
+        {
+            return LodepngWrapper::save_image(filename);
+        }
+    }
+
+    int get_width()
+    {
+        return _width;
+    }
+
+    int get_height()
+    {
+        return _height;
+    }
+
+    void print_kernel_execution_times()
+    {
+        std::cout << "    Grayscale kernel execution time: " << kernel_execution_times[0] << " us\n";
+        std::cout << "    Resize kernel execution time: " << kernel_execution_times[1] << " us\n";
+        std::cout << "    Average kernel execution time: " << kernel_execution_times[2] << " us\n";
+
+        std::cout << "    Total: " << kernel_execution_times[0] 
+                                                        + kernel_execution_times[1] 
+                                                        + kernel_execution_times[2] 
+                                                        + kernel_execution_times[3] << " us\n" << std::endl;
+    }
+
+
+    cl_mem imageBuffer = nullptr;
+    cl_mem averageBuffer = nullptr;
+    cl_mem stdDeviationBuffer = nullptr;
+private:
+    
+    std::unique_ptr<OCL_Base>& _ocl_base;
+
+    cl_event _event;
+
+    // 0 - grayscale
+    // 1 - resize
+    // 2 - average
+    unsigned long kernel_execution_times[5] = {0, 0, 0, 0, 0};
+};
+
 class OCL_Phase1
 {
 public:
     OCL_Phase1()
     {
         _ocl_base.reset(new OCL_Base());
+
+        img1.reset(new OCL_image(_ocl_base));
         
         init_programs();
         init_kernels();
@@ -40,11 +206,16 @@ public:
 
     void init_programs()
     {
+        prog = _ocl_base->CreateProgramFromFile("kernels/grayscale.cl");
+        prog_average = _ocl_base->CreateProgramFromFile("kernels/p1-moving-avg.cl");
         prog_ma = _ocl_base->CreateProgramFromFile("kernels/p1-ma.cl");
     }
 
     void init_kernels()
     {
+        _ocl_base->CreateKernelFromProgram(prog, "grayscale_rgba");
+        _ocl_base->CreateKernelFromProgram(prog, "resize");
+        _ocl_base->CreateKernelFromProgram(prog_average, "average");
         _ocl_base->CreateKernelFromProgram(prog_ma, "matrix_addition");
     }
 
@@ -72,9 +243,9 @@ public:
         cl_int status;
 
         //Setting buffers to kernel arguments
-        status = clSetKernelArg(_ocl_base->GetKernel(0), 0, sizeof(cl_mem), (void *)&aBuffer);
-        status = clSetKernelArg(_ocl_base->GetKernel(0), 1, sizeof(cl_mem), (void *)&bBuffer);
-        status = clSetKernelArg(_ocl_base->GetKernel(0), 2, sizeof(cl_mem), (void *)&rBuffer);
+        status = clSetKernelArg(_ocl_base->GetKernel(3), 0, sizeof(cl_mem), (void *)&aBuffer);
+        status = clSetKernelArg(_ocl_base->GetKernel(3), 1, sizeof(cl_mem), (void *)&bBuffer);
+        status = clSetKernelArg(_ocl_base->GetKernel(3), 2, sizeof(cl_mem), (void *)&rBuffer);
 
         size_t global_work_size[2];
         global_work_size[0] = matwidth * sizeof(int);
@@ -82,7 +253,7 @@ public:
 
         //Enqueueing kernel
         status = clEnqueueNDRangeKernel(_ocl_base->commandQueue,
-                                        _ocl_base->GetKernel(0),
+                                        _ocl_base->GetKernel(3),
                                         2,
                                         NULL,
                                         global_work_size,
@@ -91,7 +262,7 @@ public:
                                         NULL,
                                         &_event);
 
-        kernel_execution_times[0] = get_kernel_execution_time(_event, _ocl_base->commandQueue);
+        kernel_execution_times[5] = get_kernel_execution_time(_event, _ocl_base->commandQueue);
 
         //Reading result from GPU memory to main memory
         status = clEnqueueReadBuffer(_ocl_base->commandQueue,
@@ -109,18 +280,27 @@ public:
 
     void print_kernel_execution_times()
     {
-        std::cout << "OpenCL kernel execution times\n";
-        std::cout << "Matrix addition: " << kernel_execution_times[0] << " us\n\n";
+        std::cout << "OpenCL kernel execution times\n\n";
+        std::cout << "  Matrix addition: " << kernel_execution_times[5] << " us\n\n";
+
+        std::cout << "  Image1:\n";
+        img1->print_kernel_execution_times();
     }
 
+    std::unique_ptr<OCL_image> img1;
     std::unique_ptr<OCL_Base> _ocl_base;
 
 private:
+    cl_program prog;
+    cl_program prog_average;
     cl_program prog_ma;
 
     cl_event _event;
 
-    // 0 - Matrix addition
+    // 0 - Grayscale
+    // 1 - Resize
+    // 2 - Average
+    // 4 - Matrix addition
     unsigned long kernel_execution_times[5] = {0, 0, 0, 0, 0};
 };
 
@@ -140,6 +320,15 @@ OCL_Phase1 ocl_phase1;
         int run() override
         {
             unsigned error = img0.transform_to_grayscale();
+            return (int) error;
+        }
+    };
+
+    struct ResizeImage : public IProgram
+    {
+        int run() override
+        {
+            unsigned error = img0.resize_image(scaling_factor);
             return (int) error;
         }
     };
@@ -170,6 +359,15 @@ OCL_Phase1 ocl_phase1;
         int run() override
         {
             unsigned error = img0.save_image("../../output-img/im0_grey.png");
+            return (int) error;
+        }
+    };
+
+    struct SaveResizedImage : public IProgram
+    {
+        int run() override
+        {
+            unsigned error = img0.save_image("../../output-img/im0_grey_resized.png");
             return (int) error;
         }
     };
@@ -283,7 +481,63 @@ OCL_Phase1 ocl_phase1;
             return (int) error;
         }
     };
+
+    struct OclLoadImage : public IProgram
+    {
+        int run() override
+        {
+            unsigned error = ocl_phase1.img1->load_image("../../source-img/im1.png");
+            return (int) error;
+        }
+    };
+
+    struct OclTransformToGreyscale : public IProgram
+    {
+        int run() override
+        {
+            ocl_phase1.img1->transform_to_grayscale();
+            return 0;
+        }
+    };
+
+    struct OclSaveGreyscaleImage : public IProgram
+    {
+        int run() override
+        {
+            unsigned error = ocl_phase1.img1->save_image("../../output-img/im1_grey.png");
+            return (int) error;
+        }
+    };
+
+    struct OclResizeImage : public IProgram
+    {
+        int run() override
+        {
+            ocl_phase1.img1->resize_image(scaling_factor);
+            return 0;
+        }
+    };
+
+    struct OclSaveResizedImage : public IProgram
+    {
+        int run() override
+        {
+            unsigned error = ocl_phase1.img1->save_image("../../output-img/im1_grey_resized.png");
+            return (int) error;
+        }
+    };
     
+    struct OclAverage : public IProgram
+    {
+        int run() override
+        {
+            ocl_phase1.img1->average(avg_window_size);
+            ocl_phase1.img1->save_image("../../output-img/im1_grey_average_filtered.png");
+            return 0;
+        }
+    };
+
+
 
 int main()
 {
@@ -293,20 +547,35 @@ int main()
 
     sw.saveStartPoint();
 
-    // Step 3
+    //Step 1
     ProgramStopwatch Program_sw(clock);
     LoadImage loadImage;
     TransformToGreyscale transformToGreyscale;
+    ResizeImage resizeImage;
     ApplyFilter applyFilter;
     SaveGreyscaleImage saveGreyscaleImage;
+    SaveResizedImage saveResizedImage;
     SaveFilteredImage saveFilteredImage;
+
+    //Step 2
     CreateMatrices createMatrices;
     MatrixAdditionC matrixAdditionC;
     MatrixAdditionOCL matrixAdditionOCL;
     SaveMatrixCpp saveMatrixCpp;
     SaveMatrixOCL saveMatrixOCL;
 
+    //Step 3
+    OclLoadImage oclLoadImage;
+    OclTransformToGreyscale oclTransformToGreyscale;
+    OclSaveGreyscaleImage oclSaveGreyscaleImage;
+    OclResizeImage oclResizeImage;
+    OclSaveResizedImage oclSaveResizedImage;
+    OclAverage oclAverage;
+
     int result = Program_sw.runProgram(loadImage);
+    //Step 1
+    std::cout << "Step 1" << std::endl;
+
     std::cout << "Load image return result: " << result << std::endl;
     std::cout << "Elapsed time: " << Program_sw.getElapsedTime() << " us" << std::endl;
 
@@ -318,20 +587,35 @@ int main()
     std::cout << "Save greyscale image return result: " << result << std::endl;
     std::cout << "Elapsed time: " << Program_sw.getElapsedTime() << " us" << std::endl;
 
+    result = Program_sw.runProgram(resizeImage);
+    std::cout << "Resize greyscale image return result: " << result << std::endl;
+    std::cout << "Elapsed time: " << Program_sw.getElapsedTime() << " us" << std::endl;
+
+    result = Program_sw.runProgram(saveResizedImage);
+    std::cout << "Save resized image return result: " << result << std::endl;
+    std::cout << "Elapsed time: " << Program_sw.getElapsedTime() << " us" << std::endl;
+
     result = Program_sw.runProgram(applyFilter);
     std::cout << "Apply filter return result: " << result << std::endl;
     std::cout << "Elapsed time: " << Program_sw.getElapsedTime() << " us" << std::endl;
 
     result = Program_sw.runProgram(saveFilteredImage);
     std::cout << "Save filtered image return result: " << result << std::endl;
-    std::cout << "Elapsed time: " << Program_sw.getElapsedTime() << " us" << std::endl;
+    std::cout << "Elapsed time: " << Program_sw.getElapsedTime() << " us" << std::endl << std::endl;
+
+    //Step 2
+    std::cout << "Step 2" << std::endl;
 
     result = Program_sw.runProgram(createMatrices);
-    std::cout << "matrix creation: " << result << std::endl;
+    std::cout << "Creation of matrices: " << result << std::endl;
     std::cout << "Elapsed time: " << Program_sw.getElapsedTime() << " us" << std::endl;
 
     result = Program_sw.runProgram(matrixAdditionC);
     std::cout << "C++ matrix addition: " << result << std::endl;
+    std::cout << "Elapsed time: " << Program_sw.getElapsedTime() << " us" << std::endl;
+
+    result = Program_sw.runProgram(saveMatrixCpp);
+    std::cout << "C++ matrix save: " << result << std::endl;
     std::cout << "Elapsed time: " << Program_sw.getElapsedTime() << " us" << std::endl;
 
     result = Program_sw.runProgram(matrixAdditionOCL);
@@ -340,11 +624,34 @@ int main()
 
     result = Program_sw.runProgram(saveMatrixOCL);
     std::cout << "OpenCL matrix save: " << result << std::endl;
+    std::cout << "Elapsed time: " << Program_sw.getElapsedTime() << " us" << std::endl << std::endl;
+
+    //Step 3
+    std::cout << "Step 3" << std::endl;
+
+    result = Program_sw.runProgram(oclLoadImage);
+    std::cout << "OpenCl: Load image return result: " << result << std::endl;
     std::cout << "Elapsed time: " << Program_sw.getElapsedTime() << " us" << std::endl;
 
-    result = Program_sw.runProgram(saveMatrixCpp);
-    std::cout << "C++ matrix save: " << result << std::endl;
+    result = Program_sw.runProgram(oclTransformToGreyscale);
+    std::cout << "OpenCl: Transform to greyscale return result: " << result << std::endl;
     std::cout << "Elapsed time: " << Program_sw.getElapsedTime() << " us" << std::endl;
+
+    result = Program_sw.runProgram(oclSaveGreyscaleImage);
+    std::cout << "OpenCl: Save greyscale image result: " << result << std::endl;
+    std::cout << "Elapsed time: " << Program_sw.getElapsedTime() << " us" << std::endl;
+
+    result = Program_sw.runProgram(oclResizeImage);
+    std::cout << "OpenCl: Resize image result: " << result << std::endl;
+    std::cout << "Elapsed time: " << Program_sw.getElapsedTime() << " us" << std::endl;
+
+    result = Program_sw.runProgram(oclSaveResizedImage);
+    std::cout << "OpenCl: Save resized image result: " << result << std::endl;
+    std::cout << "Elapsed time: " << Program_sw.getElapsedTime() << " us" << std::endl;
+
+    result = Program_sw.runProgram(oclAverage);
+    std::cout << "OpenCl: Calculate average and save image result: " << result << std::endl;
+    std::cout << "Elapsed time: " << Program_sw.getElapsedTime() << " us" << std::endl << std::endl;
 
     sw.saveEndPoint();
     std::cout << "Total elapsed time: " << sw.getElapsedTime() << " us\n" << std::endl;
