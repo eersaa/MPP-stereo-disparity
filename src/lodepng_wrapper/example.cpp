4f5958c637e2347c20bfe1544fc3130825be2ad3
eersaa <eerik.saarinen@gmail.com>
2023-05-07 20:44:10 +0300

    Fix build errors caused by the interface changes

--- src/lodepng_wrapper/example.cpp
+++ src/lodepng_wrapper/example.cpp
@@ -5,8 +5,8 @@ int main()
     lodepng_wrapper::LodepngWrapper img0;
     unsigned error = img0.load_image("../../../source-img/im0.png");
     if (error) { return (int) error; }
+    error = img0.save_image("../../../output-img/im0.png");
     error = img0.transform_to_grayscale();
-    error = img0.save_RGBAimage("../../../output-img/im0.png");
-    error = img0.save_greyimage("../../../output-img/im0_grey.png");
+    error = img0.save_image("../../../output-img/im0_grey.png");
     return (int) error;
 }
