22fa99ec811fbdfa183382fa23bcea54c0f2b164
eersaa <eerik.saarinen@gmail.com>
2023-05-07 20:54:34 +0300

    Fix example after interface change of movingAvgFilter

--- src/utilities/example.cpp
+++ src/utilities/example.cpp
@@ -4,14 +4,15 @@ int main()
 {
     // Create one dimensional image of 3x3 pixels
     unsigned char image[9] = { 1, 1, 1, 1, 10, 1, 1, 1, 1 };
+    unsigned char outImage[9]; 
 
     // take moving average of image
-    movingAvgFilter(image, 3, 3, 3);
+    movingAvgFilter(outImage, image, 3, 3, 3);
 
     // print out image as matrix
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            std::cout << image[i * 3 + j] << " ";
+            std::cout << (int)outImage[i * 3 + j] << " ";
         }
         std::cout << std::endl;
     }
